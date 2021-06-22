4d2c4deead0562668f8474ee870112cc195dddb5
root <jonas.deboeck@student.ucll.be>
2021-06-22 14:38:21 +0000

    Fixed bug in google mobility fetcher / processor

--- data_fetch/google_mobility/google_mobility.py
+++ data_fetch/google_mobility/google_mobility.py
@@ -6,14 +6,14 @@ import os
 zipurl = "https://www.gstatic.com/covid19/mobility/Region_Mobility_Report_CSVs.zip"
 
 
-with open("data\original_files\\temp.zip", 'wb') as fd:
+with open("data/original_files/temp.zip", 'wb') as fd:
     r = requests.get(zipurl, stream=True)
     for chunk in r.iter_content(chunk_size=128):
         fd.write(chunk)
 
-with ZipFile("data\original_files\\temp.zip") as zip_file:
+with ZipFile("data/original_files/temp.zip") as zip_file:
     for member in zip_file.namelist():
         if member == "2021_BE_Region_Mobility_Report.csv":
-            zip_file.extract(member, path="data\original_files")
+            zip_file.extract(member, path="data/original_files")
 
-os.remove("data\original_files\\temp.zip")
+os.remove("data/original_files/temp.zip")
