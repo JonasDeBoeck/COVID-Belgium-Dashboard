cd333dc2b4c582db50ef06c9e16897a05aa39f2c
root <jonas.deboeck@student.ucll.be>
2021-06-22 14:32:46 +0000

    Added dynamic paths

--- algorithms/kmeans/script.py
+++ algorithms/kmeans/script.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import numpy as np
 from kmeans import Kmeans
+import os
+import re 
+
+wdir = os.getcwd()
+wdir = re.sub(r'algorithms/kmeans', '', wdir)
 
 representatives = ["WestVlaanderen", "Hainaut", "Brussels", "BrabantWallon"]
 
-representatives_info = pd.read_csv("/root/COVID-Belgium-Dashboard/data/filtered_data/KMEANS.csv")
+representatives_info = pd.read_csv(f"{wdir}/data/filtered_data/KMEANS.csv")
 
 representatives_info = representatives_info[representatives_info["PROVINCE"].isin(representatives)]
 representatives_info = representatives_info[["INFECTION_RATE", "HOSPITALISATION_RATE", "TEST_POS_PERCENTAGE"]].to_numpy()
 
-kmeans = Kmeans("/root/COVID-Belgium-Dashboard/data/filtered_data/KMEANS.csv", 4, representatives_info)
+kmeans = Kmeans(f"{wdir}/data/filtered_data/KMEANS.csv", 4, representatives_info)
 kmeans.read_csv(["INFECTION_RATE", "HOSPITALISATION_RATE", "TEST_POS_PERCENTAGE"])
 kmeans.start_clustering()
-kmeans.results_to_csv("/root/COVID-Belgium-Dashboard/data/resulted_data/kmeans")
+kmeans.results_to_csv(f"{wdir}/data/resulted_data/kmeans")
